b3ec6bc38187acb0166fb453c30a34a5228af093
unknown <>
2017-05-14 22:52:23 +0800

    Add autocomplete

--- src/main/resources/static/external/home.js
+++ src/main/resources/static/external/home.js
@@ -4,16 +4,26 @@ homeApp.controller('homeCtrl', function($scope, remoteService, $q){
 	$scope.maxRange = 1000;
 	$scope.battry = 70;
 	$scope.range = 400;
+	$scope.origin = {};
+	$scope.dest = {};
 	
 	// --------------- Block for events --------------------
 	$scope.calcPercentage = function(range, maxRange){
 		return Math.round(range / maxRange * 100);
 	}
 	$scope.navigate = function(){
+		var origin = {};
+		var dest = {};
+		localSearchByKey($scope.originKey, false).then(function(result){
+			origin = result;
+			localSearchByKey($scope.destKey, false).then(function(result){
+				dest = result;
+				console.log(origin.longitude);
+				console.log(dest.longitude);
+			});
+		});
 		
-		var origin = Point(1,2);
-		var dest = Point(1,2);
-		
+		return ;
 		remoteService.getNavigationResult(origin, dest, range * battery / 100, range).success(function(response){
 			console.log(response);
 		});
@@ -23,14 +33,106 @@ homeApp.controller('homeCtrl', function($scope, remoteService, $q){
 	
 	// TODO locate
 	map.centerAndZoom("北京",12);
+
+
+	// -------------- Block about Baidu auto complete hint -------------------------
+	var acOrigin = new BMap.Autocomplete(    //建立一个自动完成的对象
+		{"input" : "originPoint"
+		,"location" : map
+	});
+	
+	acOrigin.addEventListener("onhighlight", function(e) {  //鼠标放在下拉列表上的事件
+		acHighlight(e);
+	});
+	
+	acOrigin.addEventListener("onconfirm", function(e) {    //鼠标点击下拉列表后的事件
+		acConfirmEvent(e);
+	});
+	
+	var acDest = new BMap.Autocomplete(    //建立一个自动完成的对象
+			{"input" : "destPoint"
+			,"location" : map
+		});
+
+	acDest.addEventListener("onhighlight", function(e) {  //鼠标放在下拉列表上的事件
+		acHighlight(e);
+	});
+	
+	acDest.addEventListener("onconfirm", function(e) {    //鼠标点击下拉列表后的事件
+		acConfirmEvent(e);
+	});
+	
+	function acHighlight(e){
+		var str = "";
+		var _value = e.fromitem.value;
+		var value = "";
+		if (e.fromitem.index > -1) {
+			value = _value.province +  _value.city +  _value.district +  _value.street +  _value.business;
+		}    
+		str = "FromItem<br />index = " + e.fromitem.index + "<br />value = " + value;
+		
+		value = "";
+		if (e.toitem.index > -1) {
+			_value = e.toitem.value;
+			value = _value.province +  _value.city +  _value.district +  _value.street +  _value.business;
+		}    
+		str += "<br />ToItem<br />index = " + e.toitem.index + "<br />value = " + value;
+		G("searchResultPanel").innerHTML = str;		
+	}
+	
+	function acConfirmEvent(e){
+		var _value = e.item.value;
+		var myValue = _value.province +  _value.city +  _value.district +  _value.street +  _value.business;
+		G("searchResultPanel").innerHTML ="onconfirm<br />index = " + e.item.index + "<br />myValue = " + myValue;
+		
+		setPlace(myValue);		
+	}
+
+	function setPlace(myValue){
+		map.clearOverlays();    //清除地图上所有覆盖物
+		localSearchByKey(myValue, true);
+	}
+	
+	/**
+	 * Search key position and set the coord into point
+	 * 
+	 * @param key
+	 * @param point
+	 * @returns
+	 */
+	function localSearchByKey(key, autoViewport){
+		var defered = $q.defer();
+		function myFun(){
+			var pp = local.getResults().getPoi(0).point;    //获取第一个智能搜索的结果
+			console.log(pp);
+			
+			var point = {};
+			point.longitude = pp.lng;
+			point.latitude = pp.lat;
+			defered.resolve(point);
+			
+			map.centerAndZoom(pp, 18);
+			map.addOverlay(new BMap.Marker(pp));    //添加标注
+		}
+		var local = new BMap.LocalSearch(map, { //智能搜索
+		  onSearchComplete: myFun,
+		  autoViewport : autoViewport
+		});
+		local.search(key);
+		return defered.promise;
+	}
 	
 	
-	// Coordate
+	// 
 	function Point(longitude, latitude){
 		return {
 			longitude : longitude,
 			latitude : latitude			
 		};
+	}
+	
+	function G(id) {
+		return document.getElementById(id);
 	}
 });
 
